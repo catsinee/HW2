544620a85189750c341f67f28baa1ae61d69d5b7
catsinee <kansinee@kansinee-macbookpro2.roam.corp.google.com>
2014-06-27 18:32:42 -0700

    Initial Commit

---
+++ HW2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HW2
+//
+//  Created by Kansinee Adsanatham on 6/27/14.
+//  Copyright (c) 2014 kansinee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
